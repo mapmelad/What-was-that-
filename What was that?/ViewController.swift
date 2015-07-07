4de21c4a5f51ae8965b48963dbb2e7cc4a42b104
ilyailya <bkmz2@yandex.ru>
2015-07-07 12:09:52 +0300

    Initial Commit

---
+++ What was that?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What was that?
+//
+//  Created by Илья Железников on 07/07/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
